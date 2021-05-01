06d3dcdecfe1018fc8a22e986e87a0661cc62e92
maschuep <schuepbachmanuel@gmail.com>
2021-05-01 12:47:04 +0200

    console onput on blink

--- src/services/blink.service.ts
+++ src/services/blink.service.ts
@@ -4,31 +4,40 @@ import { Gpio, Low, High } from 'onoff';
 /**
  * registers every blink and saves it into storage
  */
-export class BlinkService{
+export class BlinkService {
 
-    public measure(){
-        const led = new Gpio(0, 'in', 'both', {debounceTimeout:10})
-        let start = 0
-        led.watch((err, value) => {
-            if(err) throw err;
-            if(value === 1){
-                start = new Date().valueOf()
-            }else if(value === 0){
-                console.log('blinked for: ', start - new Date().valueOf(), ' ms')
+    public measure() {
+        const photo = new Gpio(17, 'in', 'both', { debounceTimeout: 10 })
+        let start, stop = -1;
+        let ledOn = false;
+        let data = '';
+        photo.watch((err, value) => {
+            if (err) throw err;
+            if (value === 1 && !ledOn) {
+                start = new Date().valueOf();
+                data += start + ', ';
+                ledOn = true;
+            } else if (value === 0 && ledOn) {
+                if (stop !== -1) {
+                    data += `${start - stop}, `
+                }
+                stop = new Date().valueOf();
+                data += `${stop - start}`
+                console.log(data)
+                data=''
+                ledOn = false;
             }
         })
-
-
         process.on('SIGINT', () => {
-            led.unexport();
+            photo.unexport();
         })
     }
 
-    public emulate(intervall:number, storage:StorageService){
-        setInterval(this._generateMeasurements,intervall,storage)
+    public emulate(intervall: number, storage: StorageService) {
+        setInterval(this._generateMeasurements, intervall, storage)
     }
 
-    private _generateMeasurements(storage: StorageService){
+    private _generateMeasurements(storage: StorageService) {
         storage.createMeasurement(new Date().valueOf(), Math.floor(Math.random() * 50))
     }
 }
