c564174e9a1129d1c5e6fbfe1809d1a2cfc2e8a9
maschuep <55745327+maschuep@users.noreply.github.com>
2022-08-20 16:09:10 +0200

    Update http-notifiaction.service.ts

--- src/services/http-notifiaction.service.ts
+++ src/services/http-notifiaction.service.ts
@@ -19,13 +19,20 @@ export class HttpNotificationService {
             }
         })
     }
+    
+    /**
+     * Takes the last unsent measurements and aggreages them over before sending
+     */
 
     notifyAggregated(storage: StorageService) {
         storage.getAllUntransmitted().then(all => {
+            // read the configuration of the intervall for the aggregation
             const intervall = Number.parseInt(process.env.MEASUREMENT_INTERVALL, 10);
+            // stop if there is no new measurements
             if (!all || all.length <= 0) return;
             const startValue = all[0]
             type b = { timestamp: number, wh: number };
+            // reduce the measurements to the configured intervall
             const aggregated = all.reduce((acc: b[], curr) => {
                 const latestMeasurement = acc.pop();
                 if (latestMeasurement.timestamp + intervall >= curr.timestamp) {
@@ -37,7 +44,8 @@ export class HttpNotificationService {
                 }
                 return acc;
             }, [{ timestamp: startValue.timestamp, wh: 0 }])
-
+            
+            // send the data to the backend
             BscBackendService.sendData(JSON.stringify(aggregated))
                 .then((ans) => console.log(`${new Date().toLocaleString()} sent: ${all.length} wh`))
                 .catch((err) => {
