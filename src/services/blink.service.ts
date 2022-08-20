70fa527fcf0f4983041cfb1fe36b03c149ab4a3c
maschuep <55745327+maschuep@users.noreply.github.com>
2022-08-20 15:57:12 +0200

    Update blink.service.ts

--- src/services/blink.service.ts
+++ src/services/blink.service.ts
@@ -2,31 +2,37 @@ import { StorageService } from "./storage.service";
 import { Gpio, Low, High } from 'onoff';
 import { MeasurmentAttributes } from "../models/measurement.model";
 
+
+export class BlinkService {
+
 /**
  * registers every blink and saves it into storage
  */
-export class BlinkService {
-
-
     public measure(storage:StorageService) {
+        // configure pin
         const photo = new Gpio(17, 'in', 'both', { debounceTimeout: 30 })
         let start = -1;
         let stop = -1;
         let ledOn = false;
+        // data object to save value, blinkDuration is used to find false positives
         const data: MeasurmentAttributes ={timestamp:-1, blinkDuration:-1};
+        // listen on pin for a signal of the photoresistor
         photo.watch((err, value) => {
             if (err) throw err;
+            // if blinking starts
             if (value === 1 && !ledOn) {
                 start = new Date().valueOf();
                 data.timestamp = start ;
                 ledOn = true;
-            } else if (value === 0 && ledOn) {
-                // let since = 0;
-                // if(stop !== -1) since = start - stop
+                
+            }
+            // if blinking ends
+            else if (value === 0 && ledOn) {
+              
                 stop = new Date().valueOf();
                 data.blinkDuration = stop - start;
                 storage.createMeasurement(data)
-                // console.log(data, since)
+                
                 ledOn = false;
             }
         })
@@ -35,6 +41,9 @@ export class BlinkService {
         })
     }
 
+    /* blinking emulation for testing and developement purposes
+     *
+     */
     public emulate(intervall: number, storage: StorageService) {
         setInterval(this._generateMeasurements, intervall, storage)
     }
